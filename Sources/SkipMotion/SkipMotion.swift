8470d035f5d88f6dfb106e8bea654f7d64079dc3
marcprux <mwp1@cornell.edu>
2023-10-29 00:14:43 -0400

    Use ComposeContainer to pass Modifier through

--- Sources/SkipMotion/SkipMotion.swift
+++ Sources/SkipMotion/SkipMotion.swift
@@ -22,6 +22,21 @@ let logger: Logger = Logger(subsystem: "SkipMotion", category: "MotionView")
 
 /// A MotionView embeds an animation in the Lottie JSON format.
 public struct MotionView : View {
+    let lottieData: Data
+
+    public init(lottie lottieData: Data) {
+        self.lottieData = lottieData;
+    }
+
+    public var body: some View {
+        try! LottieMotionView(lottie: lottieData)
+        #if SKIP
+        .Compose(composectx)
+        #endif
+    }
+}
+
+private struct LottieMotionView : View {
     #if !SKIP
     let lottieAnimation: LottieAnimation
     #else
@@ -29,7 +44,7 @@ public struct MotionView : View {
     #endif
 
     /// Creates a MotionView with the data represented by the given Lottie JSON.
-    public init(lottie lottieData: Data) throws {
+    init(lottie lottieData: Data) throws {
         #if !SKIP
         self.lottieAnimation = try LottieAnimation.from(data: lottieData)
         #else
@@ -38,16 +53,19 @@ public struct MotionView : View {
     }
 
     #if !SKIP
-    public var body: some View {
+    var body: some View {
         LottieView(animation: self.lottieAnimation)
             .resizable()
             .playing(loopMode: .loop)
     }
     #else
-    @Composable public override func ComposeContent(context: ComposeContext) {
-        LottieAnimation(self.lottieComposition, 
-            modifier: Modifier.fillMaxSize(),
-            iterations: LottieConstants.IterateForever)
+    @Composable override func ComposeContent(context: ComposeContext) {
+        let contentContext = context.content()
+        ComposeContainer(modifier: context.modifier) { modifier in
+            LottieAnimation(self.lottieComposition,
+                            modifier: modifier.fillMaxSize(),
+                            iterations: LottieConstants.IterateForever)
+        }
     }
     #endif
 }
